f517131c297a1e8a5c250bb29413e5a430a7bf96
yrd123 <yash.deorah@somaiya.edu>
2022-06-26 15:49:40 +0530

    added crud requests in phonebook_api

--- phonebook_api/index.js
+++ phonebook_api/index.js
@@ -26,6 +26,20 @@ app.post("/entries/create",(request,response)=>{
     response.send("Entry Created");
 });
 
+app.delete("/entries/delete/:id",async (req, res)=>{
+    await Entry.deleteOne({"_id" : req.params.id});
+    res.send({message:"Entry deleted"});
+});
+
+
+app.put("/entries/update/:id",async (req, res)=>{
+    
+    const id = req.params.id;
+    const entry = req.body.entry;
+    await Entry.updateOne({"_id" : id},{$set:entry});
+    res.send({message: "Entry updated"});
+});
+
 app.listen(4000, ()=>{
     console.log("Server is running");
 }); 
